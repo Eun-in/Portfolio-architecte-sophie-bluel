5464d9a8e2f9a505aa3b60a10c27ee8f932abab8
Anaël Penchenat <anael.penchenat@protonmail.com>
2024-02-24 00:05:02 +0100

    Etape 1.2

--- Backend/fetch.js
+++ Backend/fetch.js
@@ -1,43 +1,96 @@
-const section = document.querySelector("#portfolio")
+const section = document.querySelector("#portfolio");
+
+export async function getDataCategories() {
+  try {
+    const response = await fetch("http://localhost:5678/api/categories");
+    const data = await response.json();
+    if (response.status === 200) {
+      const id = data.length + 1;
+      data.unshift({ id, name: "Tous" });
+      const div = document.createElement("div");
+      div.classList.add("categories");
+      const ul = document.createElement("ul");
+
+      // Utilisation d'un ensemble (Set) pour stocker les noms de catégories uniques
+      const categoryNames = new Set();
+      for (const category of data) {
+        categoryNames.add(category.name);
+      }
+      for (const categoryName of categoryNames) {
+        // const li = document.createElement("li")
+        ul.innerHTML += `<li class="filter-btn">${[categoryName]}</li>`;
+      }
+
+      div.appendChild(ul);
+      section.appendChild(div);
+
+      ajoutListener();
+    }
+  } catch (error) {
+    console.error("Error fetching data", error);
+  }
+}
+
+getDataCategories();
 
 export async function getDataWork() {
   try {
-    const response = await fetch('http://localhost:5678/api/works')
-    const data = await response.json()
-    console.log(response)
+    const response = await fetch("http://localhost:5678/api/works");
+    const data = await response.json();
+    // console.log(data);
     if (response.status === 200) {
-          // console.log('Success', data)
-          section.innerHTML = '<h2>Mes Projets</h2>';
-          const div = document.createElement("div")
-          div.classList.add("gallery")
-          for (let i = 0; i < data.length; i++) {
-          div.innerHTML +=`<figure>
+      const div = document.createElement("div");
+      div.classList.add("gallery");
+      for (let i = 0; i < data.length; i++) {
+        div.innerHTML += `<figure data-category="${data[i].categoryId}">
          	   <img src="${data[i].imageUrl}" alt="Abajour Tahina">
              <figcaption>${data[i].title}</figcaption>
-             </figure>`
-          // console.log('Success', data)
-        }
-        section.appendChild(div)
+             </figure>`;
+      }
+
+      section.appendChild(div);
     }
-    } catch (error) {
-      console.error('Error fetching data', error);
+  } catch (error) {
+    console.error("Error fetching data", error);
+  }
+}
+
+getDataWork();
+
+export async function ajoutListener() {
+  const filterBtn = document.querySelectorAll(".filter-btn");
+  for (let i = 0; i < filterBtn.length; i++) {
+    const element = filterBtn[i];
+    element.addEventListener("click", (e) => {
+      switch (e.target.textContent) {
+        case "Objets":
+          ajoutLiaison(1);
+          break;
+        case "Appartements":
+          ajoutLiaison(2);
+          break;
+        case "Hotels & restaurants":
+          ajoutLiaison(3);
+          break;
+        default:
+          ajoutLiaison(0);
+          break;
+        // code block
+      }
+    });
+  }
+}
+
+export async function ajoutLiaison(categoryId) {
+  console.log(categoryId);
+  const liaisonImg = document.querySelectorAll("#portfolio figure");
+  for (let i = 0; i < liaisonImg.length; i++) {
+    const element = liaisonImg[i];
+    element.style.display = "none";
+    if (parseInt(element.dataset.category) === categoryId || categoryId === 0) {
+      element.style.display = "block";
     }
- }
 
-getDataWork()
-
-// export async function getDataCategories() {
-//   try {
-//     const response2 = await fetch('http://localhost:5678/api/categories')
-//     const data2 = await response2.json()
-//     if (response2.status === 200) {
-//       console.log('Success', data2)
-//     } else {
-//       console.log('Serveur Error', data2)
-//     }
-//   } catch(error) {
-//     console.log('Error')
-//   }
-// }
-
-// getDataCategories()
+    console.log(element);
+  }
+}
